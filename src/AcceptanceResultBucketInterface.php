0faccf9f305e13705c3f10d39e0ab83b3c960f58
Jonathan <jonathan@kiboko.fr>
2023-03-20 15:17:19 +0100

    Update composer to php 8.2, Add a phpstan lvl 6, Add rector action with Symfony and phpunit configuration, validate cs-fixer, update php 8.2 with rector

--- src/AcceptanceResultBucketInterface.php
+++ src/AcceptanceResultBucketInterface.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Kiboko\Contract\Bucket;
 
 /**
  * @template Type
+ *
  * @extends ResultBucketInterface<Type>
  */
 interface AcceptanceResultBucketInterface extends ResultBucketInterface
