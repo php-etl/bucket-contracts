b9595871228a1c41e9da9c6d9e883a28123f4a10
Grégory Planchat <gregory@kiboko.fr>
2023-11-14 09:49:46 +0100

    Fixing PHPStan level 9 issues

--- src/ResultBucketInterface.php
+++ src/ResultBucketInterface.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 namespace Kiboko\Contract\Bucket;
 
 /**
- * @template Type
+ * @template Type of non-empty-array<array-key, mixed>|object
  */
 interface ResultBucketInterface {}
